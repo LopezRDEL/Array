1d67126548309f28dd729b9db509d22a93a4c33e
LopezRDEL <141454267+LopezRDEL@users.noreply.github.com>
2026-02-27 21:47:53 -0500

    Array unidimensional

---
+++ arrayEdades/src/main/java/com/mycompany/arrayedades/ArrayEdades.java
@@ -0,0 +1,65 @@
+
+
+package com.mycompany.arrayedades;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LopezRDEL
+ */
+public class ArrayEdades {
+
+    public static void main(String[] args) {
+        int i, Lon=1;
+        
+        Scanner lector = new Scanner (System.in);
+        
+        String nombre [] = new String [Lon];
+        
+        String estado [] = new String [Lon];
+                
+        String sexo [] = new String [Lon]; 
+        
+        int edad [] = new int [Lon];
+        
+        
+        for (i = 0; i < Lon; i++) {
+         
+            System.out.println("Ingrese el nombre de la persona ");
+            nombre[i] = lector.next();
+            
+            System.out.println("Ingrese el sexo de la persona F = Femenino M = Masculino ");
+            sexo [i] = lector.next();
+            
+            
+            System.out.println("Ingrese la edad de la persona ");
+            edad [i] = lector.nextInt();
+            
+            if (edad [i] < 12) {
+                estado [i] = "La persona es un infante";
+            } 
+            else {
+                if (edad [i] < 18) {
+                    estado [i] = "La persona es adolecente ";
+                } else {
+                    if (edad [i] <62 ) {
+                        estado [i] = "La persona es adulta ";
+                    } else {
+                        estado [i] = "La persona tiene una longevidad elevada";
+                    }
+                    System.out.println("El estado de la persona es " );
+                    
+                }
+            }
+            
+            
+        }
+            for ( i = 0; i < 10; i++) {
+               
+                System.out.print(nombre[i] + "  " + sexo[i]+ "  " + edad[i]+ " " +  estado[i]);
+                
+            
+        }
+    }
+}
